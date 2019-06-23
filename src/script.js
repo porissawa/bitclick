730672afaf0ae413d37cc1447e4400d4a62a3bd4
Pedro Morissawa <pmorissawa@gmail.com>
2019-06-23 01:02:42 -0300

    changed html structure, incomplete addStructure function

--- src/script.js
+++ src/script.js
@@ -1,8 +1,14 @@
 const clicker = document.getElementById('clicker');
 const term = document.getElementById('terminal');
 const bitCounter = document.querySelector('.bit-counter > span');
-const structureSpace = document.querySelector('.structures');
-
+const toasterDiv = document.querySelector('.buy-toaster');
+const toasterCounter = document.querySelector('.buy-toaster > span');
+const mobileCounter = document.querySelector('.buy-mobile > span');
+const computerCounter = document.querySelector('.buy-computer > span');
+const serverCounter = document.querySelector('.buy-server > span');
+const supercomputerCounter = document.querySelector('.buy-supercomputer > span');
+const quantumCounter = document.querySelector('.buy-quantum > span');
+const structureSpace = document.getElementById('structures');
 
 // structures
 let structures = {
@@ -48,7 +54,24 @@ let structures = {
 class Player {
   constructor() {
     this.bits = 0;
+    this.structures = [];
   }
+
+  // addStructure(structure) {
+  //   this.bits -= structure.baseCost;
+  //   this.structures.push(structure);
+  //   switch (structure.name) {
+  //     case 'toaster':
+  //       toasterCounter.innerText += 1;
+  //       structureSpace.appendChild(addNewStructure(toaster));
+  //       break;
+  //     case 'mobile':
+  //       mobileCounter.innerText += 1;
+  //       break;
+  //     default:
+  //       break;
+  //   }
+  // }
 }
 
 class Structure {
@@ -58,10 +81,6 @@ class Structure {
     this.units = 0;
     this.hasGenerated = 0;
   }
-  
-  addStructure(structure) {
-
-  }
 }
 
 // class instatiations
@@ -69,6 +88,7 @@ const player = new Player();
 
 
 // global functions
+
 const addBits = (num) => {
   player.bits += num;
   bitCounter.innerText = player.bits;
@@ -79,5 +99,27 @@ const removeBits = (num) => {
   bitCounter.innerText = player.bits;
 };
 
+const addNewStructure = (structure) => {
+  const bought = structures[structure];
+  if (player.bits >= bought.baseCost) {
+    removeBits(bought.baseCost);
+    player.structures.push(bought);
+
+    // add to structures visible on right side
+    const newStructure = document.createElement('span');
+    newStructure.innerHTML = `${bought.name}`;
+    structureSpace.appendChild(newStructure);
+
+    // add to structure counter
+    // must make it general.
+    let counter = Number(toasterCounter.innerText);
+    counter += 1;
+    toasterCounter.innerText = counter;
+  } else {
+    alert('You lack the sufficient funds');
+  }
+};
+
 // Event listeners
 clicker.addEventListener('click', e => addBits(1));
+toasterDiv.addEventListener('click', e => addNewStructure('toaster'));
